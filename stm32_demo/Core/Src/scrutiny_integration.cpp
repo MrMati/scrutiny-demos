926f507fc9f057ca15f901448f77deb2e955e269
MrMati <mati.niedbala5@gmail.com>
2025-11-21 17:10:55 +0100

    Replace CDC with RTT

--- stm32_demo/Core/Src/scrutiny_integration.cpp
+++ stm32_demo/Core/Src/scrutiny_integration.cpp
@@ -1,5 +1,6 @@
 #include "scrutiny_integration.h"
 
+#include "SEGGER_RTT.h"
 #include "scrutiny.hpp"
 #include "usbd_cdc_if.h"
 
@@ -8,7 +9,7 @@ uint8_t scrutiny_tx_buffer[512];
 uint8_t scrutiny_datalogging_buffer[16384];
 scrutiny::VariableFrequencyLoopHandler task_idle_loop_handler("Idle");
 scrutiny::FixedFrequencyLoopHandler task_100hz_loop_handler(1e7 / 100, "100Hz");
-scrutiny::LoopHandler *scrutiny_loops[] = {
+scrutiny::LoopHandler* scrutiny_loops[] = {
     &task_idle_loop_handler,
     &task_100hz_loop_handler,
 };
@@ -22,48 +23,47 @@ static uint32_t last_timestamp_us;
 static uint32_t last_timestamp_task_100hz;
 
 void scrutiny_integration_init() {
-  config.set_buffers(scrutiny_rx_buffer, sizeof(scrutiny_rx_buffer),
-                     scrutiny_tx_buffer, sizeof(scrutiny_tx_buffer));
-  config.set_datalogging_buffers(scrutiny_datalogging_buffer,
-                                 sizeof(scrutiny_datalogging_buffer));
-  config.set_loops(scrutiny_loops,
-                   sizeof(scrutiny_loops) / sizeof(scrutiny_loops[0]));
-  config.display_name = "STM32 Demo";
+    config.set_buffers(scrutiny_rx_buffer, sizeof(scrutiny_rx_buffer),
+                       scrutiny_tx_buffer, sizeof(scrutiny_tx_buffer));
+    config.set_datalogging_buffers(scrutiny_datalogging_buffer,
+                                   sizeof(scrutiny_datalogging_buffer));
+    config.set_loops(scrutiny_loops,
+                     sizeof(scrutiny_loops) / sizeof(scrutiny_loops[0]));
+    config.display_name = "STM32 Demo";
 
-  main_handler.init(&config);
+    main_handler.init(&config);
+
+    SEGGER_RTT_Init();
 }
 
 void scrutiny_integration_update(const uint32_t timestamp_us) {
-  uint32_t timestep_100ns = 10 * (timestamp_us - last_timestamp_us);
-  last_timestamp_us = timestamp_us;
+    uint32_t timestep_100ns = 10 * (timestamp_us - last_timestamp_us);
+    last_timestamp_us = timestamp_us;
 
-  task_idle_loop_handler.process(timestep_100ns);
+    task_idle_loop_handler.process(timestep_100ns);
 
-  if (timestamp_us - last_timestamp_task_100hz >= TASK_100HZ_TIME_US) {
-    task_100hz_loop_handler.process();
-    last_timestamp_task_100hz = timestamp_us;
-  }
+    if (timestamp_us - last_timestamp_task_100hz >= TASK_100HZ_TIME_US) {
+        task_100hz_loop_handler.process();
+        last_timestamp_task_100hz = timestamp_us;
+    }
 
-  uint8_t rxTxBuf[CDC_QUEUE_MAX_PACKET_SIZE];
-  uint16_t readSize = CDC_ReceiveQueue_ReadSize(&ReceiveQueue);
-  if (readSize) {
-    if(readSize > sizeof(rxTxBuf)) readSize = sizeof(rxTxBuf);
-    CDC_ReceiveQueue_Read(&ReceiveQueue, rxTxBuf, sizeof(rxTxBuf));
-    CDC_resume_receive();
+    uint8_t rxTxBuf[256];
+    if (SEGGER_RTT_HasData(0)) {
+        unsigned int readSize = SEGGER_RTT_Read(0, rxTxBuf, sizeof(rxTxBuf));
 
-    main_handler.receive_data(rxTxBuf, readSize);
-  }
+        main_handler.receive_data(rxTxBuf, readSize);
+    }
 
-  main_handler.process(timestep_100ns);
+    main_handler.process(timestep_100ns);
 
-  unsigned int lenToSend{
-      static_cast<unsigned int>(main_handler.data_to_send())};
+    unsigned int lenToSend{
+        static_cast<unsigned int>(main_handler.data_to_send())};
 
-  if (lenToSend > 0) {
-      if (lenToSend > sizeof(rxTxBuf))   lenToSend = sizeof(rxTxBuf);
+    if (lenToSend > 0) {
+        if (lenToSend > sizeof(rxTxBuf)) lenToSend = sizeof(rxTxBuf);
 
-    main_handler.pop_data(rxTxBuf, lenToSend);
-    CDC_TransmitQueue_Enqueue(&TransmitQueue, rxTxBuf, lenToSend);
-    CDC_continue_transmit();
-  }
+        main_handler.pop_data(rxTxBuf, lenToSend);
+        
+        SEGGER_RTT_Write(0, rxTxBuf, lenToSend);
+    }
 }
